54e6ef6485374a440ec88f6ecc2efab9c4495a45
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-07-28 16:00:40 -0500

    add connection.ts for init supabase client

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_KEY } from '../config';
+
+const supabase = createClient(SUPABASE_URL as string, SUPABASE_KEY as string);
+
+export default supabase;
