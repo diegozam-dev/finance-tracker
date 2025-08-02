4c74f407cb876370edbaa7c6c497f2c29dc9b48b
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-02 06:32:53 -0500

    add errorHandler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 
 import { PORT } from './config.js';
 import router from './routes/index.js';
+import errorHandler from './middlewares/errorHandler.middleware.js';
+// import notFoundHandler from './middlewares/notFoundHandler.middleware.js';
 
 const app = express();
 
@@ -13,6 +15,10 @@ app.disable('x-powered-by');
 
 // Routes
 app.use('/', router);
+
+// Handle errors
+app.use(errorHandler);
+// app.use(notFoundHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
