977bafad8958729c9652e1d2538fbb28af0ba3a1
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-10 15:44:17 -0500

    add Register and RegisterCard

---
+++ frontend/src/modules/auth/Register.tsx
@@ -0,0 +1,12 @@
+import LoginContainer from './components/AuthContainer';
+import RegisterCard from './components/RegisterCard';
+
+const Register = () => {
+  return (
+    <LoginContainer>
+      <RegisterCard />
+    </LoginContainer>
+  );
+};
+
+export default Register;
