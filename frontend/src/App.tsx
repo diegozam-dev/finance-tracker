97df9626d9baf7eab71cbbd5d2b9f37f3575558f
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-10 10:53:23 -0500

    setup tailwindcss and material-tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,12 @@
-// import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-import { authClient } from './utils/auth-client';
+import { Button } from '@material-tailwind/react';
 
 function App() {
-  // const [count, setCount] = useState('');
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={async () => {
-            const data = await authClient.signIn.social({
-              provider: 'google'
-            });
-            console.log(data);
-          }}
-        >
-          Log in with Google
-        </button>
-        {/* <p>{count}</p> */}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-amber-700">Hola mundo</p>
+      <Button color="success">Success</Button>
     </>
   );
 }
