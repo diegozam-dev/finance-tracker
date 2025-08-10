9e6ed4b1e51df1b296368c8ad717c384c4acbe27
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-10 11:39:18 -0500

    remove unnecessary spaces

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,9 +5,7 @@ import { mtConfig } from '@material-tailwind/react';
 export default {
   content: [
     './index.html',
-
     './src/**/*.{js,ts,jsx,tsx}',
-
     './node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}'
   ],
 
