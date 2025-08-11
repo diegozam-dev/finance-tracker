2f364f910c632e37301be3bc2340652a1d1bee0f
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-11 11:28:04 -0500

    refactor loginSchema and return, and expand the width of RegisterCard

--- frontend/src/modules/auth/components/RegisterCard.tsx
+++ frontend/src/modules/auth/components/RegisterCard.tsx
@@ -1,29 +1,38 @@
-import {
-  Card,
-  Typography,
-  Button,
-  Input,
-  Checkbox
-} from '@material-tailwind/react';
+import { Card, Typography, Button, Input } from '@material-tailwind/react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { authClient } from '../../../utils/auth-client';
 import { Link } from 'react-router';
+import { type RegisterFormData } from '../types/authTypes';
 
-const loginSchema = z.object({
-  email: z.string().email({ message: 'Invalid email.' }),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters.' }),
-  remember: z.boolean().optional()
-});
-
-type LoginFormData = {
-  email: string;
-  password: string;
-  remember?: boolean;
-};
+const loginSchema = z
+  .object({
+    firstname: z
+      .string()
+      .max(16, { message: 'Limit firstname to 16 characters.' })
+      .nonempty({ message: "Can't be empty." }),
+    lastname: z
+      .string()
+      .max(16, { message: 'Limit firstname to 16 characters.' })
+      .nonempty({ message: "Can't be empty." }),
+    email: z.string().email({ message: 'Invalid email.' }),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters.' }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters.' })
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword != password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'The passwords did not match',
+        path: ['confirmPassword']
+      });
+    }
+  });
 
 const RegisterCard = () => {
   const {
@@ -34,14 +43,14 @@ const RegisterCard = () => {
     resolver: zodResolver(loginSchema)
   });
 
-  const handleSignIn = async (formData: LoginFormData) => {
-    const { email, password, remember } = formData;
+  const handleSignIn = async (formData: RegisterFormData) => {
+    const { firstname, lastname, email, password } = formData;
 
-    const { data, error } = await authClient.signIn.email({
+    const { data, error } = await authClient.signUp.email({
+      name: firstname + ' ' + lastname,
       email,
-      password,
-      // callbackURL: '/dashboard',
-      rememberMe: remember
+      password
+      // callbackURL
     });
 
     if (error) {
@@ -53,7 +62,7 @@ const RegisterCard = () => {
   };
 
   return (
-    <Card className="max-w-xs">
+    <Card className="max-w-sm">
       <Card.Header
         as={Card}
         color="primary"
@@ -64,6 +73,54 @@ const RegisterCard = () => {
         </Typography>
       </Card.Header>
       <Card.Body as="form">
+        <div className="mb-4 mt-2 space-y-1.5">
+          <Typography
+            as="label"
+            htmlFor="firstname"
+            type="small"
+            color="default"
+            className="font-semibold"
+          >
+            Firstname
+          </Typography>
+          <Input
+            id="firstname"
+            type="text"
+            placeholder="Jhon"
+            isError={Boolean(errors.firstname)}
+            color={errors.firstname ? 'error' : 'primary'}
+            {...register('firstname')}
+          />
+          {errors.firstname && (
+            <Typography type="small" color="error">
+              {errors.firstname.message}
+            </Typography>
+          )}
+        </div>
+        <div className="mb-4 mt-2 space-y-1.5">
+          <Typography
+            as="label"
+            htmlFor="lastname"
+            type="small"
+            color="default"
+            className="font-semibold"
+          >
+            Lastname
+          </Typography>
+          <Input
+            id="lastname"
+            type="text"
+            placeholder="Doe"
+            isError={Boolean(errors.lastname)}
+            color={errors.lastname ? 'error' : 'primary'}
+            {...register('lastname')}
+          />
+          {errors.lastname && (
+            <Typography type="small" color="error">
+              {errors.lastname.message}
+            </Typography>
+          )}
+        </div>
         <div className="mb-4 mt-2 space-y-1.5">
           <Typography
             as="label"
@@ -112,16 +169,32 @@ const RegisterCard = () => {
             </Typography>
           )}
         </div>
-        <div className="mb-4 flex items-center gap-2">
-          <Checkbox id="remember" {...register('remember')}>
-            <Checkbox.Indicator />
-          </Checkbox>
-          <Typography as="label" htmlFor="remember" className="text-foreground">
-            Remember Me
+        <div className="mb-4 space-y-1.5">
+          <Typography
+            as="label"
+            htmlFor="confirmPassword"
+            type="small"
+            color="default"
+            className="font-semibold"
+          >
+            Confirm password
           </Typography>
+          <Input
+            id="confirmPassword"
+            type="password"
+            placeholder="************"
+            isError={Boolean(errors.confirmPassword)}
+            color={errors.confirmPassword ? 'error' : 'primary'}
+            {...register('confirmPassword')}
+          />
+          {errors.confirmPassword && (
+            <Typography type="small" color="error">
+              {errors.confirmPassword.message}
+            </Typography>
+          )}
         </div>
         <Button type="submit" isFullWidth onClick={handleSubmit(handleSignIn)}>
-          Sign In
+          Sign Up
         </Button>
       </Card.Body>
       <Card.Footer className="text-center">
