fe4ac5ccdca7eaecab8eab2dce62f75a7a21c8ed
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-11 15:50:19 -0500

    add handleAfterAuth.ts

---
+++ backend/src/middlewares/handleAfterAuth.ts
@@ -0,0 +1,44 @@
+import dbPool from '@/db/connection.js';
+import { AuthContext, MiddlewareContext, MiddlewareOptions } from 'better-auth';
+import { createAuthMiddleware } from 'better-auth/api';
+
+const handleAfterAuth = createAuthMiddleware(async ctx => {
+  try {
+    if (ctx.path.endsWith('/sign-up/email')) {
+      handleSignUp(ctx);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const handleSignUp = async (
+  ctx: MiddlewareContext<
+    MiddlewareOptions,
+    AuthContext & {
+      returned?: unknown;
+      responseHeaders?: Headers;
+    }
+  >
+) => {
+  const client = await dbPool.connect();
+
+  const response = ctx.context.returned;
+  const user = response.user;
+
+  if (!user || !user.id) {
+    throw new Error('User creation failed in authentication system.');
+  }
+
+  const [firstname, lastname] = user.name.split(' ');
+
+  await client.query('SELECT * FROM create_profile_with_username($1, $2, $3)', [
+    user.id,
+    firstname,
+    lastname
+  ]);
+
+  client.release();
+};
+
+export default handleAfterAuth;
