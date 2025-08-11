699f778214e7f603b6be1092fd24b553b6102abc
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-11 10:15:03 -0500

    remove routes and add RouterProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
-import { Route, Routes } from 'react-router';
+import { RouterProvider } from 'react-router';
 import './App.css';
-import { Login, Register } from './modules/auth';
+import router from './routes/routes';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="auth/login" element={<Login />} />
-      <Route path="auth/register" element={<Register />} />
-      <Route path="*" element={<h1>404: Not Found</h1>} />
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
