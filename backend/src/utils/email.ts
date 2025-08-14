37519a8d1e316a07a851c96aa6a9088fffd001e7
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-13 19:41:27 -0500

    refactor sendOtpEmail with nodemailer

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,29 +1,44 @@
-import emailjs from '@emailjs/browser';
+// import { GOOGLE_CLIENT_SECRET } from '@/config.js';
 import {
-  EMAILJS_PUBLIC_KEY,
-  EMAILJS_SERVICE_ID,
-  EMAILJS_TEMPLATE_ID
+  OAUTH_CLIENT_ID,
+  OAUTH_CLIENT_SECRET,
+  OAUTH_REFRESH_TOKEN
 } from '@/config.js';
+import nodemailer from 'nodemailer';
 
-const email = async ({
+const sendOtpEmail = async ({
   userEmail,
-  url
+  otp
 }: {
   userEmail: string;
-  url: string;
+  otp: string;
 }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'diegozambrano.dev@gmail.com',
+      clientId: OAUTH_CLIENT_ID,
+      clientSecret: OAUTH_CLIENT_SECRET,
+      refreshToken: OAUTH_REFRESH_TOKEN
+    }
+  });
+
+  console.log('despues transporter');
+
+  const mailOptions = {
+    from: 'diegozambrano.dev@gmail.com',
+    to: userEmail,
+    subject: 'Verify your email address',
+    text: `Your verification code is: ${otp}`
+  };
+
   try {
-    await emailjs.send(
-      EMAILJS_SERVICE_ID!,
-      EMAILJS_TEMPLATE_ID!,
-      { userEmail, url },
-      {
-        publicKey: EMAILJS_PUBLIC_KEY
-      }
-    );
-  } catch (err) {
-    console.log('No se pudo enviar en email. Por favor inténtalo otra vez.');
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Correo enviado: ' + info.response);
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
   }
 };
 
-export default email;
+export default sendOtpEmail;
