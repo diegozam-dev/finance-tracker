60904ada4f54402f254cd46363ede3a51c938a39
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-18 20:19:02 -0500

    add methods for sendUrlToResetPassword and sendConfirmation

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -38,3 +38,67 @@ export const sendOtp = async ({
     console.error('Error al enviar el correo:', error);
   }
 };
+
+export const sendUrlToResetPassword = async ({
+  userEmail,
+  url
+}: {
+  userEmail: string;
+  url: string;
+}) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'diegozambrano.dev@gmail.com',
+      clientId: OAUTH_CLIENT_ID,
+      clientSecret: OAUTH_CLIENT_SECRET,
+      refreshToken: OAUTH_REFRESH_TOKEN
+    }
+  });
+
+  const mailOptions = {
+    from: 'diegozambrano.dev@gmail.com',
+    to: userEmail,
+    subject: 'Reset your password',
+    text: `Click the link to reset your password: ${url}`
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Correo enviado: ' + info.response);
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+  }
+};
+
+export const sendConfirmation = async ({
+  userEmail
+}: {
+  userEmail: string;
+}) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'diegozambrano.dev@gmail.com',
+      clientId: OAUTH_CLIENT_ID,
+      clientSecret: OAUTH_CLIENT_SECRET,
+      refreshToken: OAUTH_REFRESH_TOKEN
+    }
+  });
+
+  const mailOptions = {
+    from: 'diegozambrano.dev@gmail.com',
+    to: userEmail,
+    subject: 'Reset your password',
+    text: `Your password has change`
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Correo enviado: ' + info.response);
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+  }
+};
