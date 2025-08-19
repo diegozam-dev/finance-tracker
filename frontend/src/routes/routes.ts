5c01f450d1181f6c46337241070bc687f02ff612
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-18 20:20:09 -0500

    add route for otp and send-link

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router';
 import Landing from '../modules/landing/Landing';
 import { Login, Register } from '../modules/auth';
 import Otp from '../modules/otp/Otp';
+// import ResetPassword from '../modules/resetPassword/ResetPassword';
+import SendLink from '../modules/resetPassword/SendLink';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +12,8 @@ const router = createBrowserRouter([
       { index: true, Component: Landing },
       { path: 'auth/login', Component: Login },
       { path: 'auth/register', Component: Register },
-      { path: 'auth/otp', Component: Otp }
+      { path: 'auth/otp', Component: Otp },
+      { path: 'auth/send-link', Component: SendLink }
     ]
   }
 ]);
