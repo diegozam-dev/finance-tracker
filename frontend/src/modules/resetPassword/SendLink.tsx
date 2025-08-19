74be192e40cfb27e0d56d4c170b5395beed0a12d
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-18 20:19:48 -0500

    add SendLink.tsx

---
+++ frontend/src/modules/resetPassword/SendLink.tsx
@@ -0,0 +1,32 @@
+import { Input, Button } from '@material-tailwind/react';
+import { authClient } from '../../utils/auth-client';
+import { useState } from 'react';
+
+const SendLink = () => {
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSendLink = async () => {
+    const { data, error } = await authClient.requestPasswordReset({
+      email: input,
+      redirectTo: 'http://localhost:5173/auth/reset-password'
+    });
+
+    if (error) console.log(error);
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Hola password</h1>
+      <Input onChange={handleInputChange} placeholder="email" />
+      <Button onClick={handleSendLink}>Send Code</Button>
+    </div>
+  );
+};
+
+export default SendLink;
