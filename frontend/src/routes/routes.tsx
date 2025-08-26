7526eeef7ec83aa9f7ab8078c618e663b9185e1b
Diego Zambrano <diegozambrano.dev@gmail.com>
2025-08-25 21:25:55 -0500

    refactor routing structure to include user-specific Home component and organize auth routes

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,17 +2,26 @@ import { createBrowserRouter } from 'react-router';
 import LandingPage from '../pages/LandingPage';
 import { Login, Register, Otp, SendLink, ResetPassword } from '../modules/auth';
 import NotFoundPage from '../pages/NotFoundPage';
+import Home from '../pages/Home';
+import AuthContainer from '../modules/auth/layouts/AuthContainer';
 
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
-      { index: true, Component: LandingPage },
-      { path: 'auth/login', Component: Login },
-      { path: 'auth/register', Component: Register },
-      { path: 'auth/otp', Component: Otp },
-      { path: 'auth/send-link', Component: SendLink },
-      { path: 'auth/reset-password', Component: ResetPassword },
+      { path: ':userId', Component: Home },
+      { path: 'landing-page', Component: LandingPage },
+      {
+        path: 'auth',
+        Component: AuthContainer,
+        children: [
+          { path: 'login', Component: Login },
+          { path: 'register', Component: Register },
+          { path: 'otp', Component: Otp },
+          { path: 'send-link', Component: SendLink },
+          { path: 'reset-password', Component: ResetPassword }
+        ]
+      },
       { path: '*', Component: NotFoundPage }
     ]
   }
